65a753a91e58b6d94d569d2da45e11071af78aa6
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2026-01-22 21:22:55 +0100

    run pint

--- app/Models/Collection.php
+++ app/Models/Collection.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Collection extends Model
+final class Collection extends Model
 {
     //
 }
